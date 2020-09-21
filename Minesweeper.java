043b3ec89db137dfee6218001928bddcd8098a99
Eunki-H <71096881+Eunki-H@users.noreply.github.com>
2020-09-21 16:10:19 +0900

    Add files via upload

--- Minesweeper.java
+++ Minesweeper.java
@@ -16,45 +16,94 @@ public class Minesweeper
 	
 	/****************** Constructors ********************/
 	
-	/** 
-	 * Full constructor...
+	/**
+	 * Full constructor, setting the size 10x10  of keyPlay and forPlay array 
+	 * and then, creating a key set in a given mode.
+	 * 
+	 * @param startingX	    The x value of the starting position of the keyPlay array.
+	 * @param startingY	    The y value of the starting position of the keyPlay array.
+	 * @param difficulty	The level of the game, it is chosen by the player.
 	 */
+	
+	public Minesweeper(int startingX, int startingY, char difficulty)
+	{
+		this.keyPlay = new char[10][10];
+		this.makeKeyPlay(startingX, startingY, difficulty);
+		
+		this.forPlay = new char[10][10];
+		this.initializeForPlay();
+		
+		//
+	}
 	
 	/**
 	 * Default constructor, setting the size 10x10 of keyPlay and forPlay array
-	 * and then, creating a key set.
+	 * and then, creating a key set in easy mode.
+	 * 
+	 * @param startingX	    The x value of the starting position of the keyPlay array.
+	 * @param startingY	    The y value of the starting position of the keyPlay array.
 	 */
 	
-	public Minesweeper()
+	public Minesweeper(int startingX, int startingY)
 	{
 		this.keyPlay = new char[10][10];
-		this.makeKeyPlay();
+		this.makeKeyPlay(startingX, startingY, 'e');
 		
 		this.forPlay = new char[10][10];
+		this.initializeForPlay();
 	}
 
 	
 	/********************* Methods **********************/
 	
 	/** 
-	 * This method makes a keyPlay array be filled with a random key set.
+	 * This method makes a keyPlay array be filled with a random key set, 
+	 * making the give spot blank one simultaneously.
+	 * 
+	 * @param x		    The x value of the starting position of the keyPlay array.
+	 * @param y		    The y value of the starting position of the keyPlay array.
+	 * @param level  	The level of a game the player set.
 	 */
 	
-	public void makeKeyPlay()
+	public void makeKeyPlay(int x, int y, char level)
 	{				
 		Random r = new Random();
 				
+		// Level (= number of bombs) of the game
+		int numBombs = 15;
+		
+		if (level == 'e')
+			numBombs = 15;
+		
+		if (level == 'm')
+			numBombs = 20;
+		
+		if (level == 'h')
+			numBombs = 30;
+		
 		// Bomb - set
-		for (int i = 0; i < 15; i ++)
+		for (int i = 0; i < numBombs; i ++)
 		{
 			int xBomb, yBomb;
+			boolean verifyBlank;
 			
-			do	// to spread entire 15 bombs evenly
+			do	// to spread entire bombs evenly
 			{
 				xBomb = r.nextInt(10);
 				yBomb = r.nextInt(10);
 				
-			} while (keyPlay[xBomb][yBomb] == 'X');
+				verifyBlank = true;			// to verify the first spot the user set is a blank 
+				
+				for (int j = -1; j <= 1; j++)
+				{
+					for (int k = -1; k <= 1; k++)
+					{
+						if (xBomb == x + j && yBomb == y + k)
+							verifyBlank = false;
+					}
+				}
+				
+			} while (keyPlay[xBomb][yBomb] == 'X' || verifyBlank == false);
 			
 			keyPlay[xBomb][yBomb] = 'X';
 		}
@@ -163,132 +212,37 @@ public class Minesweeper
 		}		
 	}
 	
-	/** 
-	 * This method analyzes the coordinates with a specific coordinate surrounded by them,
-	 * and then, open those coordinates of forPlay under the specific conditions.
-	 * 
-	 * @param x		    The x value of the starting position of the keyPlay array
-	 * @param y		    The y value of the starting position of the keyPlay array
-	 */
 
-	public void analyzeSurrounding1(int x, int y)
-	{
-		for (int i = -1; i <= 1; i++)		// (1) Nested for - loops to apply if - statements to the surrounding of a point
-		{
-			for (int k = -1; k <= 1; k++) 	// (2)
-			{
-				if (i != 0 || k != 0) 	    // (3) , except for the point itself
-				{
-					if (keyPlay[x + i][y + k] != ' ') 	// this case means, hint #s!
-						forPlay[x + i][y + k] = keyPlay[x + i][y + k];
-				}
-			}
-		}
-	}
-	
-	/** 
-	 * This second method analyzes the coordinates with a specific coordinate surrounded by them,
-	 * and then, save all important information to the hybrid array.
+	/**
+	 * This method helps show the result when the user clicks blank ' ' spot.
+	 * It makes every blank touched by the spot open, and it also opens the all # hints surrounded by those blanks.
 	 * 
-	 * @param x		    The x value of the starting position of the keyPlay array
-	 * @param y		    The y value of the starting position of the keyPlay array
-	 * @return The indicators of the number of blanks with x, y coordinates
-	 */
+	 * @param x The x coordinate of each choice, which is blank, the user made.
+	 * @param y The y coordinate of each choice, which is blank, the user made.
+	 */	
+	
+	public void doBlankDfsSearch(int x, int y)			// Got helped from Bro. Matt
+	{
+		if (x > -1 && y > -1 && x < 10 && y < 10 && forPlay[x][y] == '-')
+		{
+				if (keyPlay[x][y] == ' ')
+				{
+					forPlay[x][y] = keyPlay[x][y];
+					doBlankDfsSearch(x - 1, y - 1);
+					doBlankDfsSearch(x - 1, y + 1);
+					doBlankDfsSearch(x - 1, y);
+					doBlankDfsSearch(x, y + 1);
+					doBlankDfsSearch(x, y - 1);
+					doBlankDfsSearch(x + 1, y - 1);
+					doBlankDfsSearch(x + 1, y + 1);
+					doBlankDfsSearch(x + 1, y);
+				}
+				
+				else if (keyPlay[x][y] != 'X')
+					forPlay[x][y] = keyPlay[x][y];
+		}		
+	}
 
-	public int[][][] analyzeSurrounding2(int x, int y)
-	{
-		int[][][] hybrid = new int[2][9][2];	// x, y coordinates - number of blanks - mode 0/1 (general/num_blanks)
-		int numBlanks = 0;
-		
-		for (int i = -1; i <= 1; i++)		
-		{
-			for (int k = -1; k <= 1; k++) 	
-			{
-				if (i != 0 || k != 0) 	  
-				{
-					if (keyPlay[x + i][y + k] == ' ') 
-					{	
-						hybrid[0][numBlanks][0]= x + i;		// to save x coordinates
-						hybrid[1][numBlanks][0]= y + k;		// to save y coordinates
-						numBlanks++;
-					}
-				}
-				
-				else	// *** to save one's own
-				{
-					hybrid[0][8][0] = x;
-					hybrid[1][8][0] = y;
-				}
-			}
-		}
-		
-		hybrid[1][1][1]= numBlanks;			// to save the number of blanks information
-		
-		return hybrid;
-	}
-	
-	public int[][][] doBlanks(int x, int y)
-	{
-		int[][][] indicators = new int[2][9][2];
-		
-		forPlay[x][y] = keyPlay[x][y]; 	
-		analyzeSurrounding1(x, y);	
-		indicators = analyzeSurrounding2(x, y); 		 	
-	
-			/*
-			for (int i = 0; i < indicators[1][1][1]; i++)
-			{
-				int newX = indicators[0][i][0], newY = indicators[1][i][0];		
-						
-				if (forPlay[newX][newY] == '-')
-				{	
-					forPlay[newX][newY] = keyPlay[newX][newY]; 			// (1)
-					analyzeSurrounding1(newX, newY);		     	    // (2)
-					
-				}
-			}
-			*/
-		
-		return indicators;
-	}
-	/*
-		analyzeSurrounding1(x, y);
-		int[][][] indicators = analyzeSurrounding2(x, y);
-		int numBlanks = indicators[1][1][1];
-		
-		boolean forWhile = false;
-		
-		if (numBlanks == 0) 	// (1) to terminate when the number of blanks is 0
-			forWhile = true;
-		
-		else
-		{
-			int preX = -1, preY = -1;	// impossible #s, -1
-			
-			do
-			{	
-				if (indicators[0][9][0] == preX && indicators[1][9][0] == preY)     // (2) to terminate when all blanks are opened
-					forWhile = true;
-				
-				else	
-				{	
-					numBlanks = indicators[1][1][1];
-				
-					for(int i = 0; i < numBlanks; i++)
-						analyzeSurrounding1(indicators[0][i][0], indicators[1][i][0]);
-					
-					preX = indicators[0][9][0];
-					preY = indicators[1][9][0];
-					
-					// new 
-					if (numBlanks - 1 == )
-					indicators = analyzeSurrounding2(indicators[0][numBlanks - 1][0], indicators[1][numBlanks - 1][0]);
-				}
-				
-			} while (forWhile == false);
-		} 
-	*/
-	
 	/** 
 	 * This method shows the result of a player's choice, one by one.
 	 * 
@@ -296,93 +250,31 @@ public class Minesweeper
 	 * @param y The y coordinate of each choice the user made.
 	 */
 	
-	
-	public void doBlankDfsSearch(int x, int y)
-	{
-		if (x > -1 && y > -1 &&
-			x <= 10 && y <= 10 &&
-			this.forPlay[x][y] == '-'){
-
-				if (this.keyPlay[x][y] == ' '){
-					this.forPlay[x][y] = this.keyPlay[x][y];
-					doBlankDfsSearch(x-1,y+1);
-					doBlankDfsSearch(x,y+1);
-					doBlankDfsSearch(x+1,y+1);
-					doBlankDfsSearch(x+1,y);
-					doBlankDfsSearch(x+1,y-1);
-					doBlankDfsSearch(x,y-1);
-					doBlankDfsSearch(x-1,y-1);
-				}
-				else if (this.keyPlay[x][y] != 'X'){
-					this.forPlay[x][y] = this.keyPlay[x][y];
-				}
-		}		
-	}
-
-
-	
-
 	public void showByOne(int x, int y)
 	{
-		if (keyPlay[x][y] == ' ')
+		if (forPlay[x][y] != '-')
 		{
-			doBlankDfsSearch(x,y);
-			/*
-			int[][][] reference = doBlanks(x, y);
+			System.out.println();
+			System.out.println("You alreday had that spot.");
+			System.out.println("Try again!");
+		}
 			
-			for (int i = 0; i < reference[1][1][1]; i++)
-			{
-				int newX = reference[0][i][0], newY = reference[1][i][0];
-				
-				if (forPlay[newX][newY] == '-')
-				{
-					int[][][] reference2 = doBlanks(newX, newY);
-				}
-			}
-			*/		
+		else if (keyPlay[x][y] == ' ')
+		{	
+			doBlankDfsSearch(x,y);	// Continuous reveal of the spots around that blank!
 			
-			/*	
-			forPlay[x][y] = keyPlay[x][y]; 		// (1) to reveal that blank
-			analyzeSurrounding1(x, y);			// (2) to open only the hint #s around the given position 
-			int[][][] indicators = analyzeSurrounding2(x, y); 		// (3) to get information of blanks around it
+			clearScreen(50);
 			
-			int i = 0;
-			
-			for (int k = 0; k < indicators[1][1][1]; k++)
-			{
-				int newX = indicators[0][i][0], newY = indicators[1][i][0];		
-					
-				if (forPlay[newX][newY] == '-')
-				{	
-					forPlay[newX][newY] = keyPlay[newX][newY]; 			// (1)
-					analyzeSurrounding1(newX, newY);					// (2)
-					int[][][] dup = analyzeSurrounding2(newX, newY);	// (3)
-					
-					for (int q = 0; q < 2; q++)
-					{
-						for (int w = 0; w < 9; w++)
-						{
-							for (int e = 0; e < 2; e++)
-								indicators[q][w][e] = dup[q][w][e];
-						}
-					}
-					
-					k = 0;
-					i = 0;
-				}
-				
-				else i++;
-			}
-			*/
-		
-			showForPlay();		// to be cont'd with forPlay array
+			showForPlay();		// to be cont'd 
 		}		
 		
 		else if (keyPlay[x][y] == 'X')
 		{
-			forPlay[x][y] = keyPlay[x][y]; 		// Bomb!!
+			forPlay[x][y] = keyPlay[x][y]; 		// Bomb!!!
 			
-			showForPlay();		
+			clearScreen(50);
+			
+			showForPlay();	
 			
 			// The game ends w/ a question for the ask of the key
 			
@@ -390,34 +282,112 @@ public class Minesweeper
 			
 			System.out.println();
 			System.out.println("YOU FAILED - Do you want the rest to be opened? (y/n)");
-			String reply = console.next();
+			char reply = console.next().charAt(0);
 			
-			if (reply == "y")
+			if (reply == 'y')
 			{
+				clearScreen(50);
+				
 				showKeyPlay();
+				System.out.println("\n      - Game Over - ");
 				System.exit(-1);
 			}
 			else
+			{
 				System.exit(-1);	// to terminate the program
+				System.out.println("\n      - Game Over - ");
+			}
 		}		
 		
-		else
+		else if (keyPlay[x][y] > 0 + 48 && keyPlay[x][y] < 9 + 48)
 		{
 			forPlay[x][y] = keyPlay[x][y]; 		// to reveal that hint #
 			
-			showForPlay();		
+			clearScreen(50);
 			
+			showForPlay();		// to be cont'd
 		}		
 	}
 	
-
+	/** 
+	 * This method put a flag into the given spot of the forPlay array.
+	 * -> flag = bomb (the player thinks)
+	 * 
+	 * @param x The x coordinate for the flag spot.
+	 * @param y The y coordinate for the flag spot.
+	 */
 	
+	public void doFlag(int x, int y)
+	{
+		if (forPlay[x][y] == '-')
+		{
+			forPlay[x][y] = 'p';
+			
+			clearScreen(50);
+			
+			showForPlay();		// to be cont'd
+		}
+		
+		else
+		{
+			System.out.println();
+			System.out.println("You alreday done that spot.");
+			System.out.println("Try again!");
+		}
+		
+	}
 	
+	/** 
+	 * This method make a decision for the player about if the player wins a game,
+	 * when the player finds out all of the bombs.
+	 * 
+	 * @return The decision if the user finished the game with one's success.
+	 */
 	
+	public boolean winOrNot()
+	{
+		boolean win = false;
+		int verifytTotal = 0;
+		
+		for (int row = 0; row < 10; row++)
+	    {
+			for (int col = 0; col < 10; col++)
+			{
+				if (forPlay[row][col] == keyPlay[row][col] && keyPlay[row][col] != 'X')
+					verifytTotal++;
+			}
+	 	}
+		
+		if (verifytTotal == 100 - 15)
+			win = true;
+		
+		return win;
+	}
 	
+	/** 
+	 * This method sends the player the winner's message!
+     * and then, the program ends.
+	 */
 	
+	public void winningMessage()
+	{
+		System.out.println();
+		System.out.println("Congratulations!! You won the game ~ :)");
+		System.out.println("\n      - Game Over - ");
+		System.exit(-1);
+	}
 	
+	/** 
+	 * This method performs like clearing all the contents in the screen.
+	 *  - it is used when the next behavior the player takes.
+	 *  
+	 * @param num The number of rows taking part of the screen.
+	 */
 	
-	
+	public void clearScreen(int num)
+	{
+		for (int i = 0; i < num; i++)
+			System.out.println();
+	}
 	
 }
--- MinesweeperProgram.java
+++ MinesweeperProgram.java
@@ -5,38 +5,187 @@ public class MinesweeperProgram
 	public static void main(String[] args) 
 	{
 		// Let's just play a game!
+	
 		
-		Minesweeper play1 = new Minesweeper();		// A key set is already made for play1
-		
-		play1.initializeForPlay();
-		play1.showForPlay();
-		
+		// (0) Ask of a game difficulty
 		Scanner console = new Scanner(System.in);
 		
-		play1.showKeyPlay();
+		System.out.println(" <Eunki's Minesweeper Game>");
+		System.out.println();
+		System.out.println("\t Easy - e");
+		System.out.println("\t Medium - m");
+		System.out.println("\t Hard - h");
+		System.out.println();
+		System.out.println(" Enter a game mode you want");
+		System.out.print(" >> ");
+		char mode = console.next().charAt(0);
 		
-		// User Input
+		while (mode != 'e' && mode != 'm' && mode != 'h') // input validation
+		{
+			System.out.println();
+			System.out.println("Error - The mode you type should be \'e\', \'m\', or \'h\'");
+			System.out.print("Try again >> ");
+			mode = console.next().charAt(0);
+		}
 		
-		int x, y;
+		clearScreen(50);
+		
+		// (1) Show the first figure of a game to the player
+		char[][] forPlay = new char[10][10]; 		// just for this time
+		
+		initializeForPlay(forPlay);
+		showForPlay(forPlay);		
+		
+		// (2) The 1st. User Input
+		int Xi, Yi;
 		
 		System.out.println();
 		System.out.println("Enter a (x, y) location.");
 		
 		System.out.print("x >> ");
-		x = console.nextInt();
-		
-		// input validation, x
+		Xi = console.nextInt();
 		
 		System.out.print("y >> ");
-		y = console.nextInt();
+		Yi = console.nextInt();
 		
-		// input validation, y
-		
-		// Link 
+		// User Input Validation
+		while (Xi < 0 || Xi > 9 || Yi < 0 || Yi > 9)
+		{
+			// Error Message
+			System.out.println();
+			System.out.println("Error -- The x, y inputs should be 0 ~ 9.");
+			System.out.println("Try again!");
 			
-		play1.showByOne(x, y);
+			System.out.print("x >> ");
+			Xi = console.nextInt();
+			
+			System.out.print("y >> ");
+			Yi = console.nextInt();
+		}
 		
+		System.out.println();
+		
+		// (3) Make the key of the game & Make a game Object for the player
+		Minesweeper play1 = new Minesweeper(Xi, Yi, mode);		
+		
+		play1.clearScreen(50);
+		
+		play1.showByOne(Xi, Yi);
+		// play1.showKeyPlay();
+		
+		// (4) The Game mode!         - w/ the user -
+		while (play1.winOrNot() == false)
+		{
+			System.out.println();
+			System.out.println("Flag or Open? (\"p\" for Flag)");
+			char flagOrOpen = console.next().charAt(0);
+			
+			if (flagOrOpen == 'p')		// Flag
+			{
+				int Xp, Yp;
+				
+				System.out.println();
+				System.out.println("Enter P(x, y) location.");
+				
+				System.out.print("x >> ");
+				Xp = console.nextInt();
+				
+				System.out.print("y >> ");
+				Yp = console.nextInt();
+				
+				// User Input Validation
+				while (Xp < 0 || Xp > 9 || Yp < 0 || Yp > 9)
+				{
+					// Error Message
+					System.out.println();
+					System.out.println("Error -- The x, y inputs should be 0 ~ 9.");
+					System.out.println("Try again!");
+					
+					System.out.print("x >> ");
+					Xp = console.nextInt();
+					
+					System.out.print("y >> ");
+					Yp = console.nextInt();
+				}
+				
+				System.out.println();
+				
+				play1.doFlag(Xp, Yp);
+			}
+				
+			else						// Open
+			{
+				int x, y;
+			
+				System.out.println();
+				System.out.println("Enter a (x, y) location.");
+				
+				System.out.print("x >> ");
+				x = console.nextInt();
+				
+				System.out.print("y >> ");
+				y = console.nextInt();
+				
+				// User Input Validation
+				while (x < 0 || x > 9 || y < 0 || y > 9)
+				{
+					// Error Message
+					System.out.println();
+					System.out.println("Error -- The x, y inputs should be 0 ~ 9.");
+					System.out.println("Try again!");
+					
+					System.out.print("x >> ");
+					x = console.nextInt();
+					
+					System.out.print("y >> ");
+					y = console.nextInt();
+				}
+				
+				System.out.println();
+				
+				play1.showByOne(x, y);
+			}
+			
+		}
+		
+		// Winner's Message
+		play1.winningMessage();
 		
 	}
 
+	public static void clearScreen(int num)
+	{
+		for (int i = 0; i < num; i++)
+			System.out.println();
+	}
+	
+	public static void initializeForPlay(char[][] forPlay)
+	{		
+		for (int row = 0; row < 10; row++)		  
+			for (int col = 0; col < 10; col++)
+				forPlay[row][col] = '-';		
+	}
+	
+	public static void showForPlay(char[][] forPlay)
+	{
+		System.out.print("  "); 	
+		
+		for (int i = 0; i < 10; i++)		
+			System.out.printf(" %d", i);
+		
+		System.out.println();
+		
+		for (int i = 0; i < 10; i++)		
+		{
+			System.out.printf("%d |", i);
+			
+			for (int k = 0; k < 10; k++)
+			{	
+				System.out.printf("%c|", forPlay[i][k]);
+			}
+			
+			System.out.println();
+		}		
+	}
+	
 }
